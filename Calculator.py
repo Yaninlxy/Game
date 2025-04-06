0247eb6f470b54a4b97f65c4eb7f7faab74e1339
Yaninlxy <yaninlxy@gmail.com>
2025-04-06 18:05:40 +0300

    Фикс бага

--- Calculator.py
+++ Calculator.py
@@ -10,7 +10,7 @@ if operation == "+":
     print(f"{num1} + {num2} = {result}")
     
 elif operation == "-":
-    result = num1 + num2
+    result = num1 - num2
     print(f"{num1} - {num2} = {result}")
 
 elif operation == "*":
--- nachalo.py
+++ nachalo.py
@@ -7,21 +7,15 @@ myName = input()
 number = random.randint(1,20)
 
 print ('Я рад тебя видеть, ' + myName + ' , я загадал число от 1 до 20 попробуй угадать число')
-for guessesTaken in range (6):
-   
-    guess = input()
-    guess = int(guess)
+for guessesTaken in range(6):
+    guess = int(input())
     
     if guess < number:
-        print ("Твое число слишком маленькое.")
-        
-    if guess > number:
-        print ("Твое число слишком большое")    
-if guess == number:
-    guessesTaken = str(guessesTaken + 1)
-    print ('Отлично, ' + myName + '! Ты справился за ' + guessesTaken + ' попытки!')
-
-    
-if guess != number:
-    number = str(number)
-    print('Увы. Я загадал число ' + number +' .')
+        print("Твое число слишком маленькое.")
+    elif guess > number:
+        print("Твое число слишком большое.")
+    else:
+        print(f"Отлично, {myName}! Ты справился за {guessesTaken + 1} попыток!")
+        break
+else:
+    print(f"Увы. Я загадал число {number}.")
